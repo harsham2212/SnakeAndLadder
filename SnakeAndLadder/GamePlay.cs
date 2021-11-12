3d670dfbb9e8d5001511319dd7fe9e1317d61532
Harsh Singh <kmrharsh50@gmail.com>
2021-11-12 19:02:47 +0530

    [Harsh]Added:Initial Position At Zero SnakeAndLadder Features Implemented

---
+++ SnakeAndLadder/GamePlay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SnakeAndLadder
+{
+    // UseCase-1 : Checking player Initial Position i.e 0
+
+    public class GamePlay
+    {
+        public const int NO_PLAY = 0, SNAKE = 1, LADDER = 2;
+        public int playerPosition = 0;
+        Random rand = new Random();
+
+        public void diceRoll()
+        {
+            Console.WriteLine("InitialPosition of Player is: " + playerPosition);
+        }
+    }
+}
---
+++ SnakeAndLadder/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeAndLadder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello to Snake And Ladder game");
+            GamePlay playerl = new GamePlay();
+            playerl.diceRoll();
+        }
+    }
+}
